249682d4174f515b054e9dbe0395c037108f8283
Jon Jessee <tjessee7624@gmail.com>
2020-10-09 18:52:27 -0600

    Completed bid process to successfully update the database.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
-
+//MySQL setup
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -11,6 +11,7 @@ const connection = mysql.createConnection({
     database: 'myauction_db'
 });
 
+//Initializes application and collects user input
 function start() {
     inquirer.prompt([
         {
@@ -18,7 +19,8 @@ function start() {
             type: 'list',
             choices: [
                 'POST',
-                'BID'
+                'BID',
+                'EXIT'
             ],
             message: 'Would you like to post a new item or bid on an existing one?'
         }
@@ -30,8 +32,12 @@ function start() {
             bid();
         } else process.end();
     })
+    .catch(err => {
+        if(err) throw err;
+    });
 };
 
+//POST new items to the database
 function post() {
     inquirer.prompt([
         {
@@ -52,14 +58,72 @@ function post() {
     ])
     .then(data => {
         connection.query(
-            'INSERT INTO auction_items'
-        )
-    })
+            'INSERT INTO auction_items SET ?',
+            {
+                item: data.item,
+                value: data.value,
+                category: data.category
+            }
+        ),
+        (err, res) => {
+            if(err) throw err;
+        };
+        start();
+    });
 };
 
+//Bid on items that are already in the database
 function bid() {
+    let query = 'SELECT * FROM auction_items';
+    connection.query(query, (err, res) => {
+        if(err) throw err;
 
+        inquirer.prompt([
+            {
+                name: 'choose',
+                type: 'rawlist',
+                choices: function() {
+                    let choiceArr = [];
+                    for(i = 0; i < res.length; i++) {
+                        choiceArr.push(res[i].item);
+                    };
+                    return choiceArr;
+                },
+                message: 'Please select the item you would like to bid on:'
+            },
+            {
+                name: 'bid',
+                type: 'input',
+                message: 'How much would you like to bid for this item?'
+            }
+        ])
+        .then(data => {
+            let chosenItem = '';
+            res.forEach(obj => {
+                if(obj.item === data.choose) {
+                    chosenItem = obj;
+                };
+            });
+
+            if(chosenItem.bid < parseInt(data.bid)) {
+                connection.query(
+                    `UPDATE auction_items SET bid=${data.bid} WHERE id=${chosenItem.id}`,
+                    (err, res) => {
+                        if(err) throw err;
+                        console.log('Bid successfully updated');
+                        start();
+                    }
+                );
+            } else {
+                console.log('The amount you bid was too low');
+                bid();
+            };
+        });
+    });
 };
+
+
+start();
 
 connection.connect(err => {
     if(err) throw err;
