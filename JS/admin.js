6df836a52f96201ba17420763a59d567f4e668f9
Jon Jessee <tjessee7624@gmail.com>
2021-04-26 13:46:13 -0600

    Wrote new function to update the admin password as needed.

--- JS/admin.js
+++ JS/admin.js
@@ -22,81 +22,116 @@ const connection = mysql.createConnection({
 //Admin access is revoked for a given user
 //=========================================//
 async function revokeAdmin() {
-    let toRemove = await inquirer.prompt([
-        {
-            name: 'user',
-            type: 'input',
-            message: 'Enter the username of the individual',
-            validate: checkValue
-        }
-    ]);
-
-    let id = await queryPromise('SELECT userid FROM users WHERE ?', {username: toRemove.user});
-    if(id.length === 0) {
-        let tryAgain = await inquirer.prompt([
+    try {
+        let toRemove = await inquirer.prompt([
             {
-                name: 'attempt',
-                type: 'confirm',
-                message: 'Could not locate record with the given username. Try again?'
+                name: 'user',
+                type: 'input',
+                message: 'Enter the username of the individual',
+                validate: checkValue
             }
         ]);
-
-        (tryAgain ? await revokeAdmin() : console.log('No problem!'));
-    } else {
-        await queryPromise('UPDATE users SET admin_access = false WHERE ?', {userid: id[0].userid});
-        await queryPromise('DELETE FROM admin_users WHERE ?', {userid: id[0].userid});
-        console.log('The users\' admin access has been successfully revoked.');
-    }
+    
+        let id = await queryPromise('SELECT userid FROM users WHERE ?', {username: toRemove.user});
+        if(id.length === 0) {
+            let tryAgain = await inquirer.prompt([
+                {
+                    name: 'attempt',
+                    type: 'confirm',
+                    message: 'Could not locate record with the given username. Try again?'
+                }
+            ]);
+    
+            (tryAgain ? await revokeAdmin() : console.log('No problem!'));
+        } else {
+            await queryPromise('UPDATE users SET admin_access = false WHERE ?', {userid: id[0].userid});
+            await queryPromise('DELETE FROM admin_users WHERE ?', {userid: id[0].userid});
+            console.log('The users\' admin access has been successfully revoked.');
+        }
+    } catch(err) {
+        console.log(err);
+    };
 };
 
 //Block a user for violation of rules
 //=====================================//
 async function removeUser() {
-    let toDelete = await inquirer.prompt([
-        {
-            name: 'user',
-            type: 'input',
-            message: 'Enter the username of the individual',
-            validate: checkValue
-        }
-    ]);
-
-    let id = await queryPromise('SELECT userid FROM users WHERE ?', {username: toDelete.user});
-    if(id.length === 0) {
-        let tryAgain = await inquirer.prompt([
+    try {
+        let toDelete = await inquirer.prompt([
             {
-                name: 'attempt',
-                type: 'confirm',
-                message: 'Could not locate record with the given username. Try again?'
+                name: 'user',
+                type: 'input',
+                message: 'Enter the username of the individual',
+                validate: checkValue
+            }
+        ]);
+    
+        let id = await queryPromise('SELECT userid FROM users WHERE ?', {username: toDelete.user});
+        if(id.length === 0) {
+            let tryAgain = await inquirer.prompt([
+                {
+                    name: 'attempt',
+                    type: 'confirm',
+                    message: 'Could not locate record with the given username. Try again?'
+                }
+            ]);
+    
+            (tryAgain ? await removeUser() : console.log('No problem!'));
+        } else {
+            await queryPromise('DELETE FROM users WHERE ?', {username: toDelete.user});
+            console.log('The users\' account has been deleted.');
+        }
+    } catch(err) {
+        console.log(err);
+    };
+};
+
+
+//Chaneg password for admin users
+//================================//
+async function changePassword() {
+    try {
+        let newPass = await inquirer.prompt([
+            {
+                name: 'pass',
+                type: 'input',
+                message: 'Please input the new password for admin users:'
             }
         ]);
 
-        (tryAgain ? await removeUser() : console.log('No problem!'));
-    } else {
-        await queryPromise('DELETE FROM users WHERE ?', {username: toDelete.user});
-        console.log('The users\' account has been deleted.');
-    }
+        await queryPromise('UPDATE admin_users SET ?', {admin_password: newPass.pass});
+        console.log('The admin access password has been updated!');
+    } catch(err) {
+        console.log(err);
+    };
 };
 
 module.exports = {
     manageUsers:
     async function() {
-        let choice = await inquirer.prompt([
-            {
-                name: 'init',
-                type: 'list',
-                choices: ['Revoke admin access for a user', 'Remove a users\' account'],
-                message: 'Select an action to take:'
-            }
-        ]);
-
-        switch(choice.init) {
-            case 'Revoke admin access for a user':
-                await revokeAdmin();
-                break;
-            case 'Remove a users\' account':
-                await removeUser();
-                break;
+        try {
+            let choice = await inquirer.prompt([
+                {
+                    name: 'init',
+                    type: 'list',
+                    choices: ['Revoke admin access for a user', 'Remove a users\' account', 'Update the admin password'],
+                    message: 'Select an action to take:'
+                }
+            ]);
+    
+            switch(choice.init) {
+                case 'Revoke admin access for a user':
+                    await revokeAdmin();
+                    break;
+                case 'Remove a users\' account':
+                    await removeUser();
+                    break;
+                case 'Update the admin password':
+                    await changePassword();
+                    break;
+            };
+        } catch(err) {
+            console.log(err);
         };
     }
 };
